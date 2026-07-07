8a83ccf6cad1bf018919363f8462a813882b5609
saly <faye02saly@gmail.com>
2026-07-07 06:02:07 +0000

    initialisation

---
+++ model/chauffeur.model.php
@@ -0,0 +1,10 @@
+<?php
+
+$chauffeurs = [
+    0 => [
+        'nomComplet' => 'idriss', 'telephone' => 77657908, 'noteMoyenne' => 8, 'disponibilite' => ''
+    ],
+    1 => [
+        'nomComplet' => 'papa', 'telephone' => 776598712, 'noteMoyenne' => 6, 'disponibilite' => ''
+    ],
+];
