28b4ff6812432c0114dfdbfc45fa0f79310b8d04
saly <faye02saly@gmail.com>
2026-07-07 05:28:15 +0000

    initialisation

---
+++ model/passager.model.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$passagers = [
+    0 => [
+        'nomComplet' => 'Mami', 'email' => 'mami12@gmail.com', 'telephone' => 771001212
+    ],
+    1 => [
+        'nomComplet' => 'Seynabou', 'email' => 'seynabou12@gmail.com', 'telephone' => 778001010
+    ],
+];
+
+
+
