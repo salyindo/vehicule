d4ef43dcce00982beeacfa24f61fe4bbfdb7cdee
saly <faye02saly@gmail.com>
2026-07-07 06:04:09 +0000

    initialisation

--- model/passager.model.php
+++ model/passager.model.php
@@ -3,10 +3,10 @@
 
 $passagers = [
     0 => [
-        'nomComplet' => 'Mami', 'email' => 'mami12@gmail.com', 'telephone' => 771001212
+        'nomComplet' => 'aysha', 'email' => 'aysha7777@gmail.com', 'telephone' => 779806542
     ],
     1 => [
-        'nomComplet' => 'Seynabou', 'email' => 'seynabou12@gmail.com', 'telephone' => 778001010
+        'nomComplet' => 'Saly', 'email' => 'saly02@gmail.com', 'telephone' => 779087860
     ],
 ];
 
